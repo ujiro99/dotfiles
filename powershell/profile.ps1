d5e3e9f198f29957ffb91923d86ce05ec6ef6803
ujiro99 <siro.cola@gmail.com>
2013-08-19 13:33:58 +0900

    Powershell対応 - Powershell用設定ファイル追加 - セットアップスクリプトを修正

---
+++ powershell/profile.ps1
@@ -0,0 +1,57 @@
+# プロンプト
+# See http://gist.github.com/180853 for gitutils.ps1.
+. (Resolve-Path ~/Documents/WindowsPowershell/gitutils.ps1)
+
+function prompt {
+
+  # ディレクトリの表示
+  $pwd = $(get-location)
+  write-host ("[" + $pwd + "]") -nonewline -foregroundcolor Cyan
+  write-host " " -nonewline
+
+  # Gitリポジトリがあった場合
+  if (isCurrentDirectoryGitRepository) {
+
+     $branch = $(gitBranchName)
+     # ブランチ名の表示を右寄せする
+     $oldposition = $host.ui.rawui.CursorPosition
+     $Endline = $oldposition
+     # 一行の行数からブラケットとブランチ名の長さを引く
+     $Endline.X = 80 - 2 - $branch.Length
+     $host.ui.rawui.CursorPosition = $Endline
+
+     if ($(gitStatus) -eq 1) {
+       write-host ("[" + $branch + "]") -nonewline -foregroundcolor Red
+     } else {
+       write-host ("[" + $branch + "]") -nonewline -foregroundcolor Blue
+     }
+
+     $host.ui.rawui.CursorPosition = $oldposition
+  }
+
+  write-host ""
+  return "$ "
+}
+
+function subl { &"${Env:ProgramFiles}\Sublime Text 3\sublime_text.exe" $args }
+
+# function for alias
+function SetLocationToDesktop { set-location ~/Desktop }
+function SetLocationUp1 { set-location ../ }
+function SetLocationUp2 { set-location ../../ }
+function SetLocationHome { set-location $HOME }
+function ForceKill { taskkill /f /im }
+
+# alias
+sal l   ls
+sal g   git
+sal s   svn
+sal c   cd
+sal cdd SetLocationToDesktop
+sal ch  SetLocationHome
+sal a   adb
+sal u   SetLocationUp1
+sal uu  SetLocationUp2
+sal st  start
+sal fk  ForceKill
+sal vg  vagrant
--- setup.bat
+++ setup.bat
@@ -12,5 +12,7 @@ mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
 mklink %USERPROFILE%\.vrapperrc %USERPROFILE%\dotfiles\.vrapperrc
 mklink %USERPROFILE%\.zshrc %USERPROFILE%\dotfiles\.zshrc
 mklink %USERPROFILE%\.gtagsrc %USERPROFILE%\dotfiles\.gtagsrc
+mklink %USERPROFILE%\Documents\WindowsPowerShell\profile.ps1 %USERPROFILE%\dotfiles\powershell\profile.ps1
+mklink %USERPROFILE%\Documents\WindowsPowerShell\gitutils.ps1 %USERPROFILE%\dotfiles\powershell\gitutils.ps1
 mklink /d %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\vimfiles
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /d "doskey /macrofile=%USERPROFILE%\dotfiles\.cmdrc"
