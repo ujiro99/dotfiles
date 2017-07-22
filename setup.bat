047f6740c9b9856755080649f7923032b45bd033
unknown <ujiro99>
2017-07-22 22:13:35 +0900

    Create directory for vim, and install plugins.

--- setup.bat
+++ setup.bat
@@ -9,6 +9,14 @@ mklink %USERPROFILE%\.gitignore %USERPROFILE%\dotfiles\.gitignore
 mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
 mklink %USERPROFILE%\.vrapperrc %USERPROFILE%\dotfiles\.vrapperrc
 mklink %USERPROFILE%\.gtagsrc %USERPROFILE%\dotfiles\.gtagsrc
+
 mklink /d %USERPROFILE%\.vim %USERPROFILE%\dotfiles\vimfiles
+mkdir %USERPROFILE%/.vim/backup
+mkdir %USERPROFILE%/.vim/swap
+mkdir %USERPROFILE%/.vim/undo
+git submodule init
+git submodule update
+
 reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /d "doskey /macrofile=%USERPROFILE%\dotfiles\.cmdrc"
+
 pause
