07d16ade00d7ccff515364d20aa027c8b8b3b0b1
ujiro99 <siro.cola@gmail.com>
2023-01-27 00:09:05 +0900

    remove: 不要な設定群を削除

--- setup.bat
+++ setup.bat
@@ -3,10 +3,7 @@ mkdir %USERPROFILE%/dotfiles/vimfiles/bundle
 
 mklink %USERPROFILE%\_vimrc %USERPROFILE%\dotfiles\_vimrc
 mklink %USERPROFILE%\_gvimrc %USERPROFILE%\dotfiles\_gvimrc
-mklink %USERPROFILE%\.nyagos %USERPROFILE%\dotfiles\.nyagos
-mklink %USERPROFILE%\.vsvimrc %USERPROFILE%\dotfiles\.vsvimrc
 mklink %USERPROFILE%\.gitconfig %USERPROFILE%\dotfiles\.gitconfig
-mklink %USERPROFILE%\.vrapperrc %USERPROFILE%\dotfiles\.vrapperrc
 mklink %USERPROFILE%\.gtagsrc %USERPROFILE%\dotfiles\.gtagsrc
 mklink %USERPROFILE%\.bashrc %USERPROFILE%\dotfiles\.bashrc
 
@@ -19,7 +16,5 @@ mkdir %USERPROFILE%/.vim/undo
 git clone https://github.com/github/gitignore.git
 go get github.com/motemen/ghq
 go get github.com/junegunn/fzf
-
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /d "doskey /macrofile=%USERPROFILE%\dotfiles\.cmdrc"
 
 pause
