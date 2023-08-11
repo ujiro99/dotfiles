c63e6482ce51259c7d634a3ed340a32cdc49ef8d
ujiro99 <siro.cola@gmail.com>
2023-08-11 13:44:26 +0900

    update: フォーマッターの設定を修正

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,136 +1,143 @@
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim' -- Packer can manage itself
-  use 'duane9/nvim-rg'
-  use 'glidenote/memolist.vim'
-  use 'junegunn/vim-easy-align'
-  use 'drmingdrmer/vim-toggle-quickfix'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim") -- Packer can manage itself
+	use("duane9/nvim-rg")
+	use("glidenote/memolist.vim")
+	use("junegunn/vim-easy-align")
+	use("drmingdrmer/vim-toggle-quickfix")
 
-  use({
-      "kylechui/nvim-surround",
-      tag = "*",
-      config = function()
-          require("nvim-surround").setup({})
-      end
-  })
+	use({
+		"kylechui/nvim-surround",
+		tag = "*",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	})
 
-  use {'dinhhuy258/git.nvim',
-    config = function() require('git').setup {} end
-  }
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = function() require('gitsigns').setup {} end
-  }
+	use({
+		"dinhhuy258/git.nvim",
+		config = function()
+			require("git").setup({})
+		end,
+	})
+	use({
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	})
 
-  use {
-    "windwp/nvim-autopairs",
-    config = function() require("nvim-autopairs").setup {} end
-  }
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
-  use 'lambdalisue/fern.vim'
-  use {
-    'TheLeoP/fern-renderer-web-devicons.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons' }
-  }
+	use("lambdalisue/fern.vim")
+	use({
+		"TheLeoP/fern-renderer-web-devicons.nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+	})
 
-  -- appearance
-  use({
-    "neanias/everforest-nvim",
-    config = function()
-      require("everforest").setup()
-    end,
-  })
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-    config = function()
-      require 'lualine'.setup {
-        options = {
-          icons_enabled = false,
-          theme = 'everforest',
-          section_separators = '',
-          component_separators = ''
-        }
-      }
-    end
-  }
+	-- appearance
+	use({
+		"neanias/everforest-nvim",
+		config = function()
+			require("everforest").setup()
+		end,
+	})
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = false,
+					theme = "everforest",
+				},
+			})
+		end,
+	})
 
-  -- telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
-  use {
-    "nvim-telescope/telescope-frecency.nvim",
-    commit = '62d3381',
-    config = function()
-      require "telescope".load_extension("frecency")
-    end,
-    requires = { "kkharji/sqlite.lua" }
-  }
-  use {
-    'nvim-telescope/telescope-ghq.nvim',
-    config = function()
-      require 'telescope'.load_extension 'ghq'
-    end,
-  }
-  use {
-    'delphinus/telescope-memo.nvim',
-    config = function()
-      require 'telescope'.load_extension 'memo'
-    end,
-  }
+	-- telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		requires = { "nvim-lua/plenary.nvim" },
+	})
+	use({
+		"nvim-telescope/telescope-frecency.nvim",
+		commit = "62d3381",
+		config = function()
+			require("telescope").load_extension("frecency")
+		end,
+		requires = { "kkharji/sqlite.lua" },
+	})
+	use({
+		"nvim-telescope/telescope-ghq.nvim",
+		config = function()
+			require("telescope").load_extension("ghq")
+		end,
+	})
+	use({
+		"delphinus/telescope-memo.nvim",
+		config = function()
+			require("telescope").load_extension("memo")
+		end,
+	})
 
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use { "hrsh7th/nvim-cmp",
-    requires = {
-      "hrsh7th/cmp-nvim-lsp",
-      {
-        'L3MON4D3/LuaSnip',
-        tag = "v1.*",
-        requires = 'saadparwaiz1/cmp_luasnip',
-        --       run = 'make install_jsregexp',
-      }
-    }
-  }
-  use {
-    'j-hui/fidget.nvim',
-    tag = 'legacy',
-    config = function() require("fidget").setup {} end,
-  }
-  use('jose-elias-alvarez/null-ls.nvim')
-  use('MunifTanjim/prettier.nvim')
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-nvim-lsp",
+			{
+				"L3MON4D3/LuaSnip",
+				tag = "v1.*",
+				requires = "saadparwaiz1/cmp_luasnip",
+				--       run = 'make install_jsregexp',
+			},
+		},
+	})
+	use({
+		"j-hui/fidget.nvim",
+		tag = "legacy",
+		config = function()
+			require("fidget").setup({})
+		end,
+	})
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("MunifTanjim/prettier.nvim")
 
-  -- Github copilot
-  use {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
-    end,
-  }
-  use {
-    "zbirenbaum/copilot-cmp",
-    after = { "copilot.lua" },
-    config = function()
-      require("copilot_cmp").setup()
-    end
-  }
+	-- Github copilot
+	use({
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	})
+	use({
+		"zbirenbaum/copilot-cmp",
+		after = { "copilot.lua" },
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	})
 
-  use { 'mattn/emmet-vim',
-    ft = { 'html', 'css', 'react' }
-  }
+	use({ "mattn/emmet-vim", ft = { "html", "css", "react" } })
 end)
--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,123 +1,154 @@
 -- 1. LSP Sever management
-require('mason').setup()
-require('mason-lspconfig').setup_handlers({ function(server)
-  require('lspconfig')[server].setup {
-    capabilities = require('cmp_nvim_lsp').default_capabilities(),
-  }
-end })
+require("mason").setup()
+require("mason-lspconfig").setup_handlers({
+	function(server)
+		require("lspconfig")[server].setup({
+			capabilities = require("cmp_nvim_lsp").default_capabilities(),
+		})
+	end,
+})
 
 -- 2. build-in LSP function
 -- keyboard shortcut
-vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
-vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
-vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
-vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-vim.keymap.set('n', '<Leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
-vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
-vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>")
+vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<CR>")
+vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+vim.keymap.set("n", "gn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+vim.keymap.set("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+vim.keymap.set("n", "<Leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>")
+vim.keymap.set("n", "g]", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+vim.keymap.set("n", "g[", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
 -- LSP handlers
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-    vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics,
-      {
-        virtual_text = false
-      }
-    )
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+	virtual_text = false,
+})
 
 -- 3. completion (hrsh7th/nvim-cmp)
 local cmp = require("cmp")
 cmp.setup({
-  sources = {
-    { name = "nvim_lsp" },
-    { name = 'luasnip' },
-    { name = "buffer" },
-    { name = "path" },
-    { name = 'copilot' },
-  },
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    -- ['<C-l>'] = cmp.mapping.complete(),
-    -- ['<C-e>'] = cmp.mapping.abort(),
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-  }),
-  experimental = {
-    ghost_text = true,
-  },
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "copilot" },
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		-- ['<C-l>'] = cmp.mapping.complete(),
+		-- ['<C-e>'] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	experimental = {
+		ghost_text = true,
+	},
 })
-
 
 -- Show diagnostic on hover
 local function on_cursor_hold()
-  if vim.lsp.buf.server_ready() then
-    vim.diagnostic.open_float()
-  end
+	if vim.lsp.buf.server_ready() then
+		vim.diagnostic.open_float()
+	end
 end
 local diagnostic_hover_augroup_name = "lspconfig-diagnostic"
-vim.api.nvim_set_option('updatetime', 500)
+vim.api.nvim_set_option("updatetime", 500)
 vim.api.nvim_create_augroup(diagnostic_hover_augroup_name, { clear = true })
 vim.api.nvim_create_autocmd({ "CursorHold" }, { group = diagnostic_hover_augroup_name, callback = on_cursor_hold })
 
-
 -- null-ls
+local async_formatting = function(bufnr)
+	bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+	vim.lsp.buf_request(
+		bufnr,
+		"textDocument/formatting",
+		vim.lsp.util.make_formatting_params({}),
+		function(err, res, ctx)
+			if err then
+				local err_msg = type(err) == "string" and err or err.message
+				-- you can modify the log message / level (or ignore it completely)
+				vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+				return
+			end
+
+			-- don't apply results if buffer is unloaded or has been modified
+			if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+				return
+			end
+
+			if res then
+				local client = vim.lsp.get_client_by_id(ctx.client_id)
+				vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+				vim.api.nvim_buf_call(bufnr, function()
+					vim.cmd("silent noautocmd update")
+				end)
+			end
+		end
+	)
+end
+
 local null_ls = require("null-ls")
-local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
-local event = "BufWritePre" -- or "BufWritePost"
-local async = event == "BufWritePost"
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-  sources = {
-    null_ls.builtins.formatting.prettier,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
+	sources = {
+		null_ls.builtins.formatting.prettierd,
+		null_ls.builtins.formatting.sql_formatter,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.fixjson,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			-- format on <Leader>f
+			vim.keymap.set("n", "<Leader>f", function()
+				vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+			end, { buffer = bufnr, desc = "[lsp] format" })
 
-      -- format on save
-      vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-      vim.api.nvim_create_autocmd(event, {
-        buffer = bufnr,
-        group = group,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
-        end,
-        desc = "[lsp] format on save",
-      })
-    end
-
-    if client.supports_method("textDocument/rangeFormatting") then
-      vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-      end, { buffer = bufnr, desc = "[lsp] format" })
-    end
-  end,
+			-- format on save
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePost", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					async_formatting(bufnr)
+				end,
+			})
+		end
+	end,
 })
-
 
 -- prettier
 local prettier = require("prettier")
 prettier.setup({
-  bin = 'prettierd',
-  filetypes = {
-    "css",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "markdown",
-    "typescript",
-    "typescriptreact",
-    "lua"
-  },
+	bin = "prettierd",
+	["null-ls"] = {
+		condition = function()
+			return prettier.config_exists({
+				check_package_json = true,
+			})
+		end,
+		runtime_condition = function(params)
+			return true
+		end,
+		timeout = 5000,
+	},
+	filetypes = {
+		"css",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"json",
+		"markdown",
+		"typescript",
+		"typescriptreact",
+	},
 })
