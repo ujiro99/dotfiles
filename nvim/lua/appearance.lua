d08d35a9edf813a29844a9b3f716d4f9ebf5df7c
ujiro99 <siro.cola@gmail.com>
2023-09-17 19:42:07 +0900

    lazy.nvimにしてみる

--- nvim/lua/appearance.lua
+++ nvim/lua/appearance.lua
@@ -8,6 +8,8 @@ vim.o.number = true -- 行番号の表示
 vim.o.list = true -- 不可視文字表示
 vim.o.listchars = "tab:»-,trail:_,extends:»,precedes:«,nbsp:%" -- 不可視文字の表示形式
 vim.o.signcolumn = "yes"
+-- vim.o.columns = 120 -- "横幅
+-- vim.o.lines = 60 -- "高さ
 
 -- カラースキーム
 vim.cmd.colorscheme("everforest")
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,143 +1,179 @@
--- Only required if you have packer configured as `opt`
-vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
 
-return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim") -- Packer can manage itself
-	use("duane9/nvim-rg")
-	use("glidenote/memolist.vim")
-	use("junegunn/vim-easy-align")
-	use("drmingdrmer/vim-toggle-quickfix")
+require("lazy").setup({
+	"duane9/nvim-rg",
+	"glidenote/memolist.vim",
+	"junegunn/vim-easy-align",
+	"drmingdrmer/vim-toggle-quickfix",
 
-	use({
+	{
 		"kylechui/nvim-surround",
-		tag = "*",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = { "VeryLazy" },
 		config = function()
 			require("nvim-surround").setup({})
 		end,
-	})
-
-	use({
-		"dinhhuy258/git.nvim",
-		config = function()
-			require("git").setup({})
-		end,
-	})
-	use({
+	},
+	{
 		"lewis6991/gitsigns.nvim",
+		event = { "BufNewFile", "BufRead" },
+		keys = {
+			{ "<C-j>d", ":Gitsigns preview_hunk<CR>" },
+			{ "<C-j>D", ":Gitsigns diffthis<CR>" },
+			{ "<C-j><C-u>", ":Gitsigns reset_hunk<CR>" },
+			{ "<Space>s", ":Gitsigns stage_hunk<CR>" },
+			{ "<Space>j", ":Gitsigns next_hunk<CR>" },
+			{ "<Space>k", ":Gitsigns prev_hunk<CR>" },
+		},
 		config = function()
-			require("gitsigns").setup({})
+			require("gitsigns").setup()
 		end,
-	})
-
-	use({
+	},
+	{
 		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	})
+		event = { "InsertEnter" },
+		opts = {},
+	},
 
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-	use("lambdalisue/fern.vim")
-	use({
-		"TheLeoP/fern-renderer-web-devicons.nvim",
-		requires = { "nvim-tree/nvim-web-devicons" },
-	})
+	{
+		"lambdalisue/fern.vim",
+		keys = {
+			{ ";n", ":Fern . -reveal=% -drawer -toggle<CR>", desc = "toggle fern" },
+		},
+		dependencies = {
+			{ "lambdalisue/nerdfont.vim" },
+			{
+				"lambdalisue/fern-renderer-nerdfont.vim",
+				config = function()
+					vim.g["fern#renderer"] = "nerdfont"
+				end,
+			},
+		},
+	},
 
 	-- appearance
-	use({
+	{
 		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
 		config = function()
-			require("everforest").setup()
+			require("everforest").setup({
+				-- Your config here
+			})
 		end,
-	})
-	use({
+	},
+	{
 		"nvim-lualine/lualine.nvim",
-		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		event = { "BufNewFile", "BufRead" },
+		opts = {
+			icons_enabled = false,
+			theme = "everforest",
+		},
 		config = function()
 			require("lualine").setup({
-				options = {
-					icons_enabled = false,
-					theme = "everforest",
+				sections = {
+					lualine_x = {
+						{
+							require("lazy.status").updates,
+							cond = require("lazy.status").has_updates,
+						},
+					},
 				},
 			})
 		end,
-	})
+	},
 
 	-- telescope
-	use({
+	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.2",
-		requires = { "nvim-lua/plenary.nvim" },
-	})
-	use({
+		tag = "0.1.3",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
 		"nvim-telescope/telescope-frecency.nvim",
-		commit = "62d3381",
 		config = function()
 			require("telescope").load_extension("frecency")
 		end,
-		requires = { "kkharji/sqlite.lua" },
-	})
-	use({
+		dependencies = { "kkharji/sqlite.lua" },
+	},
+	{
 		"nvim-telescope/telescope-ghq.nvim",
 		config = function()
 			require("telescope").load_extension("ghq")
 		end,
-	})
-	use({
+	},
+	{
 		"delphinus/telescope-memo.nvim",
 		config = function()
 			require("telescope").load_extension("memo")
 		end,
-	})
+	},
 
 	-- LSP
-	use("neovim/nvim-lspconfig")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use({
+	"neovim/nvim-lspconfig",
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	{
 		"hrsh7th/nvim-cmp",
-		requires = {
+		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			{
 				"L3MON4D3/LuaSnip",
-				tag = "v2.*",
-				run = "make install_jsregexp",
-				requires = "saadparwaiz1/cmp_luasnip",
+				keys = function()
+					return {}
+				end,
 			},
 		},
-	})
-	use({
+	},
+
+	{
 		"j-hui/fidget.nvim",
 		tag = "legacy",
-		config = function()
-			require("fidget").setup({})
-		end,
-	})
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("MunifTanjim/prettier.nvim")
+		event = { "LspAttach" },
+		opts = {},
+	},
+	"jose-elias-alvarez/null-ls.nvim",
+	"MunifTanjim/prettier.nvim",
 
 	-- Github copilot
-	use({
+	{
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
-		event = "InsertEnter",
+		event = { "InsertEnter" },
 		config = function()
 			require("copilot").setup({
 				suggestion = { enabled = false },
 				panel = { enabled = false },
 			})
 		end,
-	})
-	use({
+	},
+	{
 		"zbirenbaum/copilot-cmp",
-		after = { "copilot.lua" },
 		config = function()
 			require("copilot_cmp").setup()
 		end,
-	})
+	},
 
-	use({ "mattn/emmet-vim", ft = { "html", "css", "react" } })
-end)
+	{
+		"mattn/emmet-vim",
+		ft = { "html", "css", "react" },
+	},
+})
