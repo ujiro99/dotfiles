309333ed81f4e0590c4f34a2b4235da4c62959b7
ujiro99 <siro.cola@gmail.com>
2024-03-23 16:50:54 +0000

    raspi用に設定を修正

--- setup.linux.sh
+++ setup.linux.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
-mv ~/.bashrc ~/.bashrc.bak
-
 mkdir -p ~/dotfiles/vimfiles
 
 ln -s ~/dotfiles/.gitignore ~/.agignore
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.gtagsrc ~/.gtagsrc
-ln -s ~/dotfiles/.ctagsignore ~/.ctagsignore
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/vimfiles ~/.vim
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-
-mkdir ~/.vim/backup
-mkdir ~/.vim/swap
-mkdir ~/.vim/undo
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
-# z
-git clone --depth 1 https://github.com/rupa/z.git
-source z/z.sh
-
 # neovim
 mkdir -p ~/.config/nvim
-mkdir -p ~/.config/nvim/colors
-ln -sf ~/dotfiles/_vimrc  ~/.config/nvim/init.vim
-ln -s  ~/dotfiles/vimfiles/after/ftplugin ~/.config/nvim/ftplugin
-ln -s  ~/dotfiles/vimfiles/ftdetect ~/.config/nvim/ftdetect
-ln -s  ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
-
+ln -sf ~/dotfiles/_vimrc  ~/.config/nvim/init.lua
+ln -s ~/dotfiles/nvim/lua ~/.config/nvim/lua
+ln -s ~/dotfiles/nvim/plugin ~/.config/nvim/plugin
+ln -s ~/dotfiles/nvim/snippets ~/.config/nvim/snippets 
